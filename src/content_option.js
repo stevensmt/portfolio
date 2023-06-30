7c2bdfa5d282b4f1a72701e0aa2d76b2c1d2ff9a
stevensmt <45947137+stevensmt@users.noreply.github.com>
2023-06-30 10:02:00 +0200

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -12,7 +12,7 @@ const introdata = {
         third: "I imagine UserJourney and Level Design",
     },
     description: "Hello, I'm an UX Designer currently specialised on Immersive Experience, such as AR-VR-MR.",
-    your_img_url: "https://github.com/stevensmt/portfolio/blob/master/src/pages/home/img/illustration.png?raw=true",
+    your_img_url: "https://github.com/stevensmt/portfolio/blob/master/src/pages/home/img/4x/03.png?raw=true",
 };
 
 const dataabout = {
