0990c3f462daca36e36f7370001e6e7316018d09
stevensmt <45947137+stevensmt@users.noreply.github.com>
2023-07-03 12:30:44 +0200

    vr

--- src/pages/portfolio/VirtualReality/index.js
+++ src/pages/portfolio/VirtualReality/index.js
@@ -4,7 +4,8 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Container, Row, Col } from "react-bootstrap";
 import { 
   meta, 
-  VR, 
+  VR,
+  dataportfolio, 
 } from "../../../content_option";
 import { Link } from "react-router-dom";
 
@@ -26,95 +27,150 @@ export const VirtualReality = () => {
         <section id="home" className="home">
           <div className="intro_sec d-block d-lg-flex align-items-center ">
             <div
-              className="h_bg-image order-1 order-lg-2 h-100 "
-              style={{ backgroundImage: `url(${VR.img_link01})` }}
+              className="h_bgg-image"
+              style={{ backgroundImage: `url(${VR.Img1})` }}
             ></div>
-            <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
-              <div className="align-self-center ">
-                <div className="intro mx-auto">
-                  <h2 className="mb-1x">{VR.title}</h2>
-                  <p className="mb-1x">{VR.none}</p>
-                  <p className="mb-1x">{VR.resumeEN}</p>
-                  <p className="mb-1x"><i>{VR.resume}</i></p>
-                </div>
+          </div>
+          <div><br></br></div>
+        </section>
+
+        <section id="home" className="home">
+          <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
+            <Col lg="4">
+              <div className="intro mx-auto">
+
+                <p className="mb-1x"><b>{VR.Title01}</b></p>
+                <p className="mb-1x"><i>{VR.info01}</i></p>
+                <p className="mb-1x"><i>{VR.info012}</i></p>
+                <p className="mb-1x"><b>{VR.Title02}</b></p>
+                <p className="mb-1x"><i>{VR.info02}</i></p>
+                <p className="mb-1x"><i>{VR.info022}</i></p>
+                <p className="mb-1x"><i>{VR.info023}</i></p>
+                <p className="mb-1x"><b>{VR.Title03}</b></p>
+                <p className="mb-1x"><i>{VR.info03}</i></p>
+
               </div>
-            </div>
+            </Col>
+            <Col lg="1"></Col>
+            <Col lg="7">
+              <div className="intro mx-auto">
+                <h2 className="mb-1x">{VR.ResumeTitle}</h2>
+                <br></br>
+                <p className="mb-1x">{VR.ResumeTxt}</p>
+                <br></br>
+                <p className="mb-1x"><b>{VR.Title04}</b></p>
+                <p className="mb-1x"><i>{VR.info04}</i></p>
+                <br></br>
+              </div>
+            </Col>
+          </div>
+          <div>
+          <br></br>
+          <br></br>
+          <br></br>
           </div>
         </section>
 
         <section id="home" className="home">
           <div className="intro_sec d-block d-lg-flex align-items-center ">
-            <div className="text order-2 order-lg-2 h-200 d-lg-flex justify-content-center">
-              <div className="align-self-center ">
-                <div className="intro mx-auto">
-                  <h2 className="mb-1x">{VR.titleObj}</h2>
-                  <p className="mb-1x">{VR.none}</p>
-                  <p className="mb-1x">{VR.objectifEN}</p>
-                  <p className="mb-1x"><i>{VR.objectif}</i></p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <section id="home" className="home">
-          <div className="intro_sec d-block d-lg-flex align-items-center ">
-          <div  className="text order-1 order-lg-2 h-100 d-lg-flex justify-content-center">
-              <div className="align-self-center ">
-                <div className="intro mx-auto">
-                  <h2 className="mb-1x">{VR.title03}</h2>
-                  <p className="mb-1x">{VR.none}</p>
-                  <p className="mb-1x">{VR.ENtext03}</p>
-                  <p className="mb-1x">{VR.ENtxt33}</p>
-                  <p className="mb-1x">{VR.ENtxt34}</p>
-                  <p className="mb-1x">{VR.ENtxt35}</p>
-                  <p className="mb-1x">{VR.ENtxt36}</p>
-                  <p className="mb-1x">{VR.none}</p>
-                  <p className="mb-1x"><i>{VR.text03}</i></p>
-                  <p className="mb-1x"><i>{VR.txt33}</i></p>
-                  <p className="mb-1x"><i>{VR.txt34}</i></p>
-                  <p className="mb-1x"><i>{VR.txt35}</i></p>
-                  <p className="mb-1x"><i>{VR.txt36}</i></p>
-                </div>
-              </div>
-            </div>
             <div
-              className="h_bg-image order-2 order-lg-1 h-100 "
-              style={{ backgroundImage: `url(${VR.img_link02})` }}
+              className="h_bgg-image"
+              style={{ backgroundImage: `url(${VR.Img2})` }}
             ></div>
           </div>
         </section>
-        <Row className="mb-5 mt-3 pt-md-3">
-          <Col lg="8">
+
+        <section>
+            <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
+              <Col lg="10">
+                <div className="intro mx-auto justify-content-center">
+                  <p className="mb-1x">{VR.GoalsTxt}</p>
+                  <p className="mb-1x">{VR.GoalsTxt2}</p>
+                </div>
+              </Col>
+            </div>
+            <div>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+            </div>
+        </section>
+
+        
+
+        <section id="home" className="home">
+        <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">  
+          <Col lg="7"> 
+          <div className="intro_sec d-block d-lg-flex align-items-center "> 
+            <div
+              className="h_bgg-image order-1 order-lg-3 h-100"
+              style={{ backgroundImage: `url(${VR.Img3})` }}
+            ></div>
+            </div>
           </Col>
-        </Row>
-        <section id="home" className="home">
-          <div className="intro_sec d-block d-lg-flex align-items-center ">
-            <div
-              className="h_bg-image order-1 order-lg-2 h-100 "
-              style={{ backgroundImage: `url(${VR.img_link03})` }}
-            ></div>
-            <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
-              <div className="align-self-center ">
-                <div className="intro mx-auto">
-                  <h2 className="mb-1x">{VR.title02}</h2>
-                  <p className="mb-1x">{VR.none}</p>
-                  <p className="mb-1x">{VR.ENtext02}</p>
-                  <p className="mb-1x">{VR.ENtxt23}</p>
-                  <p className="mb-1x">{VR.ENtxt24}</p>
-                  <p className="mb-1x">{VR.ENtxt25}</p>
-                  <p className="mb-1x">{VR.ENtxt26}</p>
-                  <p className="mb-1x">{VR.none}</p>
-                  <p className="mb-1x"><i>{VR.text02}</i></p>
-                  <p className="mb-1x"><i>{VR.txt23}</i></p>
-                  <p className="mb-1x"><i>{VR.txt24}</i></p>
-                  <p className="mb-1x"><i>{VR.txt25}</i></p>
-                  <p className="mb-1x"><i>{VR.txt26}</i></p>                </div>
-              </div>
+          <Col lg="1"></Col>
+          <Col lg="4"> 
+            <div className="intro mx-auto">
+              <p className="mb-1x">{VR.DeroulementTxt}</p>
             </div>
+          </Col> 
+          </div>
+          <div>
+          <br></br>
+          <br></br>
+          <br></br>
           </div>
         </section>
 
+        <section id="home" className="home">
+        <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
+          <Col lg="4"> 
+            <div className="intro mx-auto">
+
+              <p className="mb-1x">{VR.DeroulementTxt2}</p> 
+
+            </div>
+          </Col> 
+          <Col lg="1"></Col>
+          <Col lg="7">
+          <div className="intro_sec d-block d-lg-flex "> 
+            <div
+              className="h_bgg-image order-1 order-lg-3 h-100"
+              style={{ backgroundImage: `url(${VR.Img4})` }}
+            ></div>
+            </div>
+          </Col>
+          </div>
+          <div>
+          <br></br>
+          <br></br>
+          <br></br>
+          </div>
+        </section>
+
+        <section id="home" className="home">
+          <div className="intro_sec d-block d-lg-flex align-items-center ">
+            <div
+              className="h_bgg-image"
+              style={{ backgroundImage: `url(${VR.Img5})` }}
+            ></div>
+          </div>
+          <div className="d-block d-lg-flex align-items-center ">
+            <div className="intro mx-auto">
+              <a href={VR.backLink}><b>Previous project</b></a>
+            </div>
+            <div className="intro mx-auto"></div>
+            <div className="intro mx-auto">
+              <a href={VR.nextLink}><b>Next project</b></a>
+            </div>
+          </div>
+          <div>
+            <br></br>
+            <br></br>
+            <br></br>
+         </div>
+      </section>
       </Container>
     </HelmetProvider>
   );
